daff10156ec1402a02c6e184ea5f96e11021b0fa
Mattia Ippoliti <ippolitimattia@gmail.com>
2024-02-04 12:18:09 +0100

    2024

--- MattiaIppoliti/assets/js/index-new.js
+++ MattiaIppoliti/assets/js/index-new.js
@@ -709,6 +709,49 @@ function initMagneticButtons() {
 /**
  * Sticky Cursor with Delay
  */
+
+/*
+// Second Mouse Cursor
+
+var $circle = $('.second-circle'),
+    $follow = $('.second-circle-follow');
+
+function moveCircle(e) {
+	TweenLite.to($circle, 0.3, {
+      x: e.clientX,
+      y: e.clientY
+  });
+	TweenLite.to($follow, 0.7, {
+      x: e.clientX,
+      y: e.clientY
+  });  
+}
+
+function hoverFunc(e) {
+  TweenLite.to($circle, 0.3, {
+    opacity: 1,
+    scale: 0
+  });
+  TweenLite.to($follow, 0.3, {
+      scale: 3
+  });  
+}
+
+function unhoverFunc(e) {
+  TweenLite.to($circle, 0.3, {
+    opacity: 1,
+    scale: 1
+  });
+  TweenLite.to($follow, 0.3, {
+      scale: 1
+  });  
+}
+
+$(window).on('mousemove', moveCircle);
+
+$("a").hover(hoverFunc, unhoverFunc);
+*/
+
 function initStickyCursorWithDelay() {
 
     // Sticky Cursor with delay
